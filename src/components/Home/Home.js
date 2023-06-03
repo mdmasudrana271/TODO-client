284f4c2997eb407d631f6360339d8bba651a910d
Md Abdullah Al Masud <126868744+abdullah-almasud@users.noreply.github.com>
2023-06-03 21:47:34 +0600

    modify some code

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,43 +5,50 @@ import { AuthContext } from "../../context/AuthProvider";
 
 const Home = () => {
   const [todos, setTodos] = useState([]);
-  const { user } = useContext(AuthContext);
+  const { user, logOut } = useContext(AuthContext);
 
-
-  const handleSearch =(event)=>{
+  const handleSearch = (event) => {
     event.preventDefault();
     const form = event.target;
     const search = form.search.value;
     fetch(`https://todo-app-server-side-phi.vercel.app/search?search=${search}`)
-    .then(res=> res.json())
-    .then(data=> {
-      setTodos(data)
-    })
-
-  }
-
-
-
-  useEffect(() => {
-    fetch(`https://todo-app-server-side-phi.vercel.app/all-todo?email=${user.email}`,{
-      headers: {
-        'content-type': 'application/json',
-        authorization: `Bearer ${localStorage.getItem("todoAppAccessToken")}`,
-      }
-    })
       .then((res) => res.json())
       .then((data) => {
         setTodos(data);
       });
-  }, [user.email]);
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://todo-app-server-side-phi.vercel.app/all-todo?email=${user.email}`,
+      {
+        headers: {
+          "content-type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("todoAppAccessToken")}`,
+        },
+      }
+    )
+      .then((res) => {
+        if (res.status === 401 || res.status === 403) {
+          return logOut();
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setTodos(data);
+      });
+  }, [user.email, logOut]);
 
   return (
     <div className="mt-5">
-      <form onSubmit={handleSearch} className="flex justify-center items-center mt-10">
+      <form
+        onSubmit={handleSearch}
+        className="flex justify-center items-center mt-10"
+      >
         <input
           type="text"
           placeholder="Search Todo"
-          name='search'
+          name="search"
           className="input w-full max-w-xs form-control shadow-xl"
         />
       </form>
